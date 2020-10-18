a16a18180db6cd6eabf68489e493b1e988f2ea46
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2020-10-19 00:43:14 +0300

    Update caesar.go

--- homework01-go/caesar/caesar.go
+++ homework01-go/caesar/caesar.go
@@ -3,15 +3,29 @@ package caesar
 func EncryptCaesar(plaintext string, shift int) string {
 	var ciphertext string
 
-	// PUT YOUR CODE HERE
-
+	for i := 0; i < len(plaintext); i++ {
+		symbolNumber := int(plaintext[i])
+		if (int('A') <= symbolNumber) && (symbolNumber <= int('Z')) {
+			symbolNumber = (int(symbolNumber)+shift-int('A'))%26 + int('A')
+		} else if (int('a') <= symbolNumber) && (symbolNumber <= int('z')) {
+			symbolNumber = (int(symbolNumber)+shift-int('a'))%26 + int('a')
+		}
+		ciphertext += string(rune(symbolNumber))
+	}
 	return ciphertext
 }
 
 func DecryptCaesar(ciphertext string, shift int) string {
 	var plaintext string
 
-	// PUT YOUR CODE HERE
-
+	for i := 0; i < len(ciphertext); i++ {
+		symbolNumber := int(ciphertext[i])
+		if (int('A') <= symbolNumber) && (symbolNumber <= int('Z')) {
+			symbolNumber = (symbolNumber-shift-int('A')+26)%26 + int('A')
+		} else if (int('a') <= symbolNumber) && (symbolNumber <= int('z')) {
+			symbolNumber = (symbolNumber-shift-int('a')+26)%26 + int('a')
+		}
+		plaintext += string(rune(symbolNumber))
+	}
 	return plaintext
 }
