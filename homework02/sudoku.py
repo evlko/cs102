631ed2df539539d84da6100f3188389a73199edf
evlko <levkolive@gmail.com>
2020-10-28 22:46:42 +0300

    Fix MyPy

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -97,7 +97,7 @@ def find_empty_positions(grid: List[List[str]]) -> Optional[Tuple[int, int]]:
         for j in range(len(grid[i]))
         if grid[i][j] == "."
     ]
-    return empty_positions[0] if len(empty_positions) > 0 else (-1, -1)
+    return empty_positions[0] if len(empty_positions) > 0 else None
 
 
 def find_possible_values(grid: List[List[str]], pos: Tuple[int, int]) -> Set[str]:
@@ -133,7 +133,7 @@ def solve(grid: List[List[str]]) -> Optional[List[List[str]]]:
     [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
     """
     empty_position = find_empty_positions(grid)
-    if empty_position == (-1, -1):
+    if empty_position is None:
         return grid
     possible_values = find_possible_values(grid, empty_position)
     if len(possible_values) == 0:
@@ -143,6 +143,7 @@ def solve(grid: List[List[str]]) -> Optional[List[List[str]]]:
         if solve(grid):
             return solve(grid)
         grid[empty_position[0]][empty_position[1]] = "."
+    return None
 
 
 def check_solution(solution: List[List[str]]) -> bool:
