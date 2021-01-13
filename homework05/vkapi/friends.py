148ad8312dbb2030b0e1481ae2e8ea0737533471
evlko <>
2021-01-13 17:40:22 +0300

    hw5 done

--- homework05/vkapi/friends.py
+++ homework05/vkapi/friends.py
@@ -4,6 +4,7 @@ import time
 import typing as tp
 
 from vkapi import config, session
+from vkapi.config import VK_CONFIG
 from vkapi.exceptions import APIError
 
 QueryParams = tp.Optional[tp.Dict[str, tp.Union[str, int]]]
@@ -16,7 +17,10 @@ class FriendsResponse:
 
 
 def get_friends(
-    user_id: int, count: int = 5000, offset: int = 0, fields: tp.Optional[tp.List[str]] = None
+    user_id: int,
+    count: int = 5000,
+    offset: int = 0,
+    fields: tp.Optional[tp.List[str]] = None,
 ) -> FriendsResponse:
     """
     Получить список идентификаторов друзей пользователя или расширенную информацию
@@ -28,7 +32,18 @@ def get_friends(
     :param fields: Список полей, которые нужно получить для каждого пользователя.
     :return: Список идентификаторов друзей пользователя или список пользователей.
     """
-    pass
+    params = {
+        "access_token": VK_CONFIG["access_token"],
+        "v": VK_CONFIG["version"],
+        "count": count,
+        "user_id": user_id if user_id is not None else "",
+        "fields": ",".join(fields) if fields is not None else "",
+        "offset": offset,
+    }
+    response = session.get("friends.get", params=params).json()
+    if "error" in response:
+        raise APIError(response["error"]["error_msg"])
+    return FriendsResponse(**response["response"])
 
 
 class MutualFriends(tp.TypedDict):
@@ -57,4 +72,40 @@ def get_mutual(
     :param offset: Смещение, необходимое для выборки определенного подмножества общих друзей.
     :param progress: Callback для отображения прогресса.
     """
-    pass
+    if target_uids is None:
+        params = {
+            "access_token": VK_CONFIG["access_token"],
+            "v": VK_CONFIG["version"],
+            "source_uid": source_uid if source_uid is not None else "",
+            "target_uid": target_uid,
+            "order": order,
+        }
+        response = session.get("friends.getMutual", params=params).json()
+        if "error" in response:
+            raise APIError(response["error"]["error_msg"])
+        return response["response"]
+
+    results = []
+    progress = lambda x: x
+    for i in progress(range(((len(target_uids) + 99) // 100))):
+        params = {
+            "access_token": VK_CONFIG["access_token"],
+            "v": VK_CONFIG["version"],
+            "target_uids": ",".join(map(str, target_uids)),
+            "order": order,
+            "count": count if count is not None else "",
+            "offset": offset + i * 100,
+        }
+        response = session.get("friends.getMutual", params=params).json()
+        if "error" in response:
+            raise APIError(response["error"]["error_msg"])
+        for arg in response["response"]:
+            results.append(
+                MutualFriends(
+                    id=arg["id"],
+                    common_friends=arg["common_friends"],
+                    common_count=arg["common_count"],
+                )
+            )
+        time.sleep(1) if i % 3 == 2 else time.sleep(0)
+    return results
