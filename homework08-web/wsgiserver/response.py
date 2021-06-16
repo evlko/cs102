56e2549c6661a0589e588502ef02f49d010bda65
Lev Kobelev <35657934+evlko@users.noreply.github.com>
2021-06-16 19:04:25 +0300

    homework08 done

--- homework08-web/wsgiserver/response.py
+++ homework08-web/wsgiserver/response.py
@@ -5,11 +5,15 @@ from httpserver import HTTPResponse
 
 
 @dataclasses.dataclass
-class WSGIResponse(HTTPResponse):
+class WSGIResponse(HTTPResponse):  # type:ignore
     status: int = 200
 
     def start_response(
-        self, status: str, response_headers: tp.List[tp.Tuple[str, str]], exc_info=None
+        self,
+        status: str,
+        response_headers: tp.List[tp.Tuple[str, str]],
+        exc_info: tp.Optional[tp.Any] = None,
     ) -> None:
-        # Сохранить статус и заголовки ответа
-        pass
+        self.status = int(status.split(" ", 1)[0])
+        for header in response_headers:
+            self.headers.update({header[0]: header[1]})
